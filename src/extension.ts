01e12147788fc9f59df5fd69757867c24093ca57
Jad Hamza <jad.hamza@epfl.ch>
2020-02-14 16:07:03 +0100

    Add command to erase annotations

---
+++ src/extension.ts
@@ -0,0 +1,46 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+        // Use the console to output diagnostic information (console.log) and errors (console.error)
+        // This line of code will only be executed once when your extension is activated
+        console.log('Stainless Fit extension is now active!');
+
+        // The command has been defined in the package.json file
+        // Now provide the implementation of the command with registerCommand
+        // The commandId parameter must match the command field in package.json
+        let disposable = vscode.commands.registerCommand('extension.eraseTypeAnnotations', () => {
+                // The code you place here will be executed every time your command is executed
+
+		// Get the active text editor
+		let editor = vscode.window.activeTextEditor;
+
+		if (editor) {
+			let document = editor.document;
+			let selection = editor.selection;
+
+			let text = document.getText(selection);
+
+                        let annotation = /\[[^\[\]]*\]/gi
+                        while (text.includes("[")) {
+                                text = text.replace(annotation, "");
+                        }
+
+                        let emptyFunOf = /fun *of *=/gi
+                        text = text.replace(emptyFunOf, "");
+
+			editor.edit(editBuilder => {
+				editBuilder.replace(selection, text);
+			});
+		}
+        });
+
+        context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
