8d47e937a5f2c245c435cc853b18367f9a4b0248
Jad Hamza <jad.hamza@epfl.ch>
2020-02-15 22:39:36 +0100

    Better erased style

--- src/extension.ts
+++ src/extension.ts
@@ -25,13 +25,42 @@ export function activate(context: vscode.ExtensionContext) {
 
       let text = document.getText(selection);
 
-      let annotation = /\[[^\[\]]*\]/gi
-      while (text.match(annotation)) {
+      let annotation = /\[[^\[\]]*\]/g
+      while (text.match(annotation))
         text = text.replace(annotation, "");
-      }
 
-      let emptyFunOf = /fun *of *=/gi
+      let emptyFunOf = /fun *of *=/g
       text = text.replace(emptyFunOf, "");
+
+      // a non-space followed by an identifier in parentheses
+      let parenthesizedId1 = /([^\s])\((\s*[a-z]\w*\s*)\)/g
+      while (text.match(parenthesizedId1))
+        text = text.replace(parenthesizedId1, "$1 $2")
+
+      // a space followed by an identifier in parentheses
+      let parenthesizedId2 = /\s\((\s*[a-z]\w*\s*)\)/g
+      while (text.match(parenthesizedId2))
+        text = text.replace(parenthesizedId2, " $1")
+
+      // several spaces after a non-space character
+      let severalSpaces = /(\S)[ \t]+/g
+      text = text.replace(severalSpaces, "$1 ")
+
+      // trailing spaces
+      let trailingSpaces = /[ \t]+([\r\n])/g
+      text = text.replace(trailingSpaces, "$1")
+
+      // > three line returns
+      let threeLineReturns = /([\n\r][\n\r])[\n\r]+/g
+      text = text.replace(threeLineReturns, "$1")
+
+      // initial new lines
+      let initialNewLines = /^[\n\r]+/g
+      text = text.replace(initialNewLines, "")
+
+      // odd indentation
+      let oddIndentation = /([\n\r]) ((?:  )*)(\S)/g
+      text = text.replace(oddIndentation, "$1$2$3")
 
       editor.edit(editBuilder => {
         editBuilder.replace(selection, text);
