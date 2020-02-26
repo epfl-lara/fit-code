d5d446329d20058f7a23da58b4d0ac6a1c257b62
Jad Hamza <jad.hamza@epfl.ch>
2020-02-26 08:23:22 +0100

    Clean-up

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,10 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   console.log('Stainless Fit extension is now active!')
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   let eraseTypeAnnotations = vscode.commands.registerCommand('extension.eraseTypeAnnotations', () => {
-    // The code you place here will be executed every time your command is executed
-
-    // Get the active text editor
     let editor = vscode.window.activeTextEditor
 
     if (editor) {
@@ -70,53 +58,34 @@ export function activate(context: vscode.ExtensionContext) {
   });
 
   let evaluateCurrentFile = vscode.commands.registerCommand('extension.evaluateCurrentFile', () => {
-    // The code you place here will be executed every time your command is executed
-
-    // Get the active text editor
     let editor = vscode.window.activeTextEditor
 
     if (editor) {
       let document = editor.document
-
       let filename = document.fileName
-      
-      //let pattern = /[^\\\/]*\ [^\\\/]*/g
-
-      //let filename2 = filename.replace(pattern,'\'\$&\'')
 
       const execSync = require('child_process').execSync;
       const output = execSync("${fitcode.executablePath} eval --no-info \""  + filename + "\"", { encoding: 'utf-8' });
-      //console.log('Output was:\n', output);
       vscode.window.showInformationMessage("Evaluates to:\n" + output);
     }
   });
 
   let typecheckCurrentFile = vscode.commands.registerCommand('extension.typecheckCurrentFile', () => {
-    // The code you place here will be executed every time your command is executed
-
-    // Get the active text editor
     let editor = vscode.window.activeTextEditor
 
     if (editor) {
       let document = editor.document
-
       let filename = document.fileName
-      
-      //let pattern = /[^\\\/]*\ [^\\\/]*/g
-
-      //let filename2 = filename.replace(pattern,'\'\$&\'')
 
       const execSync = require('child_process').execSync;
       const output = execSync("${fitcode.executablePath} typecheck --no-info \""  + filename + "\"", { encoding: 'utf-8' });
       vscode.window.showInformationMessage(output);
     }
   });
-  
 
   context.subscriptions.push(eraseTypeAnnotations)
   context.subscriptions.push(evaluateCurrentFile)
   context.subscriptions.push(typecheckCurrentFile)
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
